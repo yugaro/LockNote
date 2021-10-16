d8fd6ee16e19c013905799a85321d50e93095ae5
yugaro <yugaro.yugaro@gmail.com>
2021-10-17 00:48:06 +0900

    Initial Commit

--- App.jsx
+++ App.jsx
@@ -11,11 +11,13 @@ import LogInScreen from './src/screens/LogInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import { firebaseConfig } from './env';
 
-const Stack = createStackNavigator();
+require('firebase/firestore');
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
   View, StyleSheet, TextInput,
 } from 'react-native';
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/KeyBoardSafeView';
 
 export default function MemoCreateScreen(props) {
   const { navigation } = props;
+  const handlePress = useCallback(() => {
+    const db = firebase.firestore();
+    const ref = db.collection('memos');
+    ref.add({
+      bodyText: 'Hello',
+    })
+      .then((docRef) => {
+        console.log('Created !', docRef.id);
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log('Error!', error);
+      });
+  });
+
   return (
     <KeyboardSafeView style={styles.containter}>
       <View style={styles.inputContainer}>
@@ -15,7 +31,7 @@ export default function MemoCreateScreen(props) {
       </View>
       <CircleButton
         name="check"
-        onPress={() => { navigation.goBack(); }}
+        onPress={handlePress}
       />
     </KeyboardSafeView>
   );
