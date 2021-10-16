a5850c65eb4aea4dc62b5a15305ae1f6edd82ad1
yugaro <yugaro.yugaro@gmail.com>
2021-10-16 13:09:53 +0900

    Initial Commit

---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  View, StyleSheet, TextInput, KeyboardAvoidingView,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoEditScreen() {
+  return (
+    <KeyboardAvoidingView style={styles.containter} behavior="height">
+      <AppBar />
+      <View style={styles.inputContainer}>
+        <TextInput value="Shopping List" multiline style={styles.input} />
+      </View>
+      <CircleButton name="check" />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  containter: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
