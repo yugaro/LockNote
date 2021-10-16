29dcc2c7381760fee42b5ab5d43a2da1bd986120
yugaro <yugaro.yugaro@gmail.com>
2021-10-16 11:55:13 +0900

    Initial Commit

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <MemoDetailScreen />
+    <MemoListScreen />
   );
 }
--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -20,7 +20,7 @@ export default function MemoDetailScreen() {
           test test test test test test test test test test
         </Text>
       </ScrollView>
-      <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
+      <CircleButton style={{ top: 160, bottom: 'auto' }} name="edit" />
     </View>
   );
 }
