217d4b4f45f25cf3a03c737f91c48f5e3ab4cc61
yugaro <yugaro.yugaro@gmail.com>
2021-10-17 15:07:55 +0900

    Initial Commit

--- src/components/SquareBotton.jsx
+++ src/components/SquareBotton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
-import { string, func } from 'prop-types';
+import { string, func, shape } from 'prop-types';
 
 export default function SquareButton(props) {
-  const { label, onPress } = props;
+  const { label, onPress, style } = props;
   return (
-    <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
+    <TouchableOpacity style={[styles.buttonContainer, style]} onPress={onPress}>
       <Text style={styles.buttonLabel}>{label}</Text>
     </TouchableOpacity>
   );
@@ -14,10 +14,12 @@ export default function SquareButton(props) {
 SquareButton.propTypes = {
   label: string.isRequired,
   onPress: func,
+  style: shape(),
 };
 
 SquareButton.defaultProps = {
   onPress: null,
+  style: null,
 };
 
 const styles = StyleSheet.create({
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
+import {
+  Text, View, StyleSheet, Alert,
+} from 'react-native';
 import firebase from 'firebase';
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 import LogOutButton from '../components/LogOutButton';
+import SquareButton from '../components/SquareBotton';
+import Loading from '../components/Loading';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
   const [memos, setMemos] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => <LogOutButton />,
@@ -20,6 +26,7 @@ export default function MemoListScreen(props) {
     const db = firebase.firestore();
     let unsubscribe = () => {};
     if (currentUser) {
+      setLoading(true);
       const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
       // Monitor memos
       unsubscribe = ref.onSnapshot((snapshot) => {
@@ -34,13 +41,31 @@ export default function MemoListScreen(props) {
           });
         });
         setMemos(userMemos);
+        setLoading(false);
       }, (error) => {
         console.log(error);
         Alert.alert('Fail to load data.');
+        setLoading(false);
       });
     }
     return unsubscribe;
   }, []);
+
+  if (memos.length === 0) {
+    return (
+      <View style={emptyStyles.container}>
+        <Loading isLoading={isLoading} />
+        <View style={emptyStyles.inner}>
+          <Text style={emptyStyles.title}>Let&apos;s create your first memo !</Text>
+          <SquareButton
+            style={emptyStyles.button}
+            label="create"
+            onPress={() => { navigation.navigate('MemoCreate'); }}
+          />
+        </View>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -57,5 +82,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
+  },
+});
+
+const emptyStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inner: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 24,
+  },
+  button: {
+    alignSelf: 'center',
   },
 });
